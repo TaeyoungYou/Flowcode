0b03d1a24895f014f8b23dcea4545fa0a2f2aae3
Gukbap <taeyoung.you12@gmail.com>
2025-01-29 03:10:20 -0500

    Done implementation and test json

--- A12/Reader.h
+++ A12/Reader.h
@@ -70,31 +70,30 @@ enum READER_MODE {
 
 #define READER_DEFAULT_SIZE			250		/* default initial buffer reader capacity */
 #define READER_DEFAULT_INCREMENT	10		/* default increment factor */
-#define READER_DEFAULT_FLAG			0x00	/* default flag value */
 
 #define NCHAR				128			/* Chars from 0 to 127 */
 
 #define CHARSEOF			(-1)		/* EOF Code for Reader */
 
-/* STRUCTURES DEFINITION: SUFIXED BY LANGUAGE NAME (SOFIA) .................................. */
+/* STRUCTURES DEFINITION: SUFIXED BY LANGUAGE NAME (FlowCode) .................................. */
 
 /* TODO: Adjust datatypes */
 
 /* Offset declaration */
 
 /* BITS(7654.3210)*/
-#define READER_DEFAULT_FLAG 0x00 /* (0000.0000)_2 = (000)_10 */
-#define FUL 0x01 /* TO_DO: BIT 0: FUL = Buffer full flag  (0000.0001) */
-#define EMP 0x02 /* TO_DO: BIT 1: EMP = Buffer empty flag (0000.0010) */
-#define REL 0x04 /* TO_DO: BIT 2: REL = Rellocation memory flag (0000.0100) */
-#define END 0x08 /* TO_DO: BIT 3: END = End of buffer flag (0000.1000) */
+#define READER_DEFAULT_FLAG 0b0000 /* (0000.0000)_2 = (000)_10 */
+#define FUL 0b0001 /* TO_DO: BIT 0: FUL = Buffer full flag  (0000.0001) */
+#define EMP 0b0010 /* TO_DO: BIT 1: EMP = Buffer empty flag (0000.0010) */
+#define REL 0b0100 /* TO_DO: BIT 2: REL = Rellocation memory flag (0000.0100) */
+#define END 0b1000 /* TO_DO: BIT 3: END = End of buffer flag (0000.1000) */
 
-/* need to be deleted */
+/* Flag declaration - being changed to use */
 typedef struct flag {
 	flowcode_bool isEmpty;			/* indicates if the buffer is empty */
 	flowcode_bool isFull;			/* indicates if the buffer is full */
-	flowcode_bool isRead;			/* indicates if the buffer was completely read */
-	flowcode_bool isMoved;			/* indicates if the buffer memory was changed */
+	flowcode_bool isRel;			/* indicates if the buffer is reallocation */
+	flowcode_bool isEnd;			/* indicates if the buffer is end point */
 } Flag;
 
 /* Offset declaration */
@@ -110,7 +109,7 @@ typedef struct bufferReader {
 	flowcode_int	size;				/* current dynamic memory size (in bytes) allocated to character buffer */
 	flowcode_int	increment;			/* character array increment factor */
 	flowcode_char	mode;				/* operational mode indicator */
-	flowcode_byte	flags;				/* contains flag bits */
+	Flag			flags;				/* contains flag bits */
 	Position		positions;			/* Offset / position field */
 	flowcode_int	histogram[NCHAR];	/* Statistics of chars */
 	flowcode_int	numReaderErrors;	/* Number of errors from Reader */
